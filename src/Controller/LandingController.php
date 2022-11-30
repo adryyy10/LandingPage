5dde16d0425ed6ca73fdcaa12b784e6dca15f0d9
adryyy10 <adriafigueresgarcia@gmail.com>
2022-11-30 12:55:39 +0000

    Create Controller + template

---
+++ src/Controller/LandingController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class LandingController extends AbstractController
+{
+
+    /**
+     * @Route("/landing", name="app_landing")
+     */
+    public function getLandingPage(): Response
+    {
+        return $this->render('landing.html.twig', []);
+    }
+
+}
